1c16a595e193ee65dc135c6f6725c7548b641c41
Hamilton Tobon Mosquera <htobonmm7@gmail.com>
2022-05-03 13:12:49 -0500

    feat: create brightness matrix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const RGB_LENGTH = 3;
 async function transform() {
   const { pixelArray, width, height } = await readRawPixels();
   const pixelMatrix = createPixelMatrix(pixelArray, width, height);
-  const brightnessMatrix = extractBrightnessMatrix(pixelMatrix);
+  const brightnessMatrix = extractBrightnessMatrix(pixelMatrix, averagePixelFilter);
+  const asciiMatrix = convertToASCIICharacters(brightnessMatrix);
 }
 
 async function readRawPixels() {
@@ -15,7 +16,7 @@ async function readRawPixels() {
 
   const { width, height } = info;
   console.log('Successfully loaded image!');
-  console.log(`Width: ${width}, Height: ${height}.`);
+  console.log(`Width: ${width}, Height: ${height}`);
 
   return {
     pixelArray: new Uint8ClampedArray(data.buffer),
@@ -28,25 +29,52 @@ function createPixelMatrix(pixelArray, width, height) {
   const pixelMatrix = new Array(height);
   for (let i = 0; i < height; i++) {
 
-    const pixelRow = new Array(width);
-    const offset = i * width;
+    pixelMatrix[i] = new Array();
+    const pixelsPerRow = width * RGB_LENGTH;
+    const rowOffset = i * pixelsPerRow;
 
-    for (let j = 0; j < width - RGB_LENGTH; j += RGB_LENGTH) {
-      const pixelStartIndex = offset + j;
+    for (let j = 0; j < pixelsPerRow; j += RGB_LENGTH) {
+      const pixelStartIndex = rowOffset + j;
       const pixel = pixelArray.slice(pixelStartIndex, pixelStartIndex + RGB_LENGTH);
-      pixelRow[j] = pixel;
+      pixelMatrix[i].push(pixel);
     }
-
-    pixelMatrix[i] = pixelRow;
-
   }
+
+  console.log('Successfully created pixel matrix!');
+  console.log(`Columns: ${pixelMatrix[0].length}, Rows: ${pixelMatrix.length}`);
 
   return pixelMatrix;
 }
 
-function extractBrightnessMatrix(pixelMatrix) {
-  console.log('Implement this brightness stuff');
-  // TODO: Maybe map reduce in each pixel.
+function averagePixelFilter(pixel) {
+  const sum = pixel.reduce((acc, val) => acc + val);
+  return sum / RGB_LENGTH;
+}
+
+function lightnessPixelFilter(pixel) {
+  return (Math.max(pixel) + Math.min(pixel)) / 2;
+}
+
+function luminosityPixelFilter(pixel) {
+  return 0.21 * pixel[0] + 0.72 * pixel[1] + 0.07 * pixel[2];
+}
+
+function extractBrightnessMatrix(pixelMatrix, pixelFilter) {
+  const height = pixelMatrix.length;
+  const brightnessMatrix = new Array(height);
+
+  for (let i = 0; i < height; i++) {
+    brightnessMatrix[i] = pixelMatrix[i].map(pixel => pixelFilter(pixel));
+  }
+
+  console.log('Successfully created brightness matrix!');
+  console.log(`Columns: ${brightnessMatrix[0].length}, Rows: ${brightnessMatrix.length}`);
+
+  return brightnessMatrix;
+}
+
+function convertToASCIICharacters(brightnessMatrix) {
+  console.log('Implement conversion stuff!!');
   return [[]];
 }
 
